78e7eb372ad3a78c173f1183b0b4d3469effcb5f
Joey Liew <joeyl@tutanota.com>
2020-09-17 01:07:18 +0200

    Add post_media table

---
+++ server/src/database/migrations/20200917010347_createPostMediaTable.ts
@@ -0,0 +1,20 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('post_media', (table) => {
+    table.increments();
+    table
+      .integer('post_id')
+      .references('id')
+      .inTable('post')
+      .unsigned()
+      .notNullable();
+    table.enum('media_type', ['video', 'image']).notNullable();
+    table.string('media_url').notNullable();
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists('post_media');
+}
